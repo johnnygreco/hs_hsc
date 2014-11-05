c1d956d5011fd203eaae0b957157ac8d9b12e364
Song Huang <dr.guangtou@gmail.com>
2014-11-05 23:10:52 +0900

    matchFakeStars.py

---
+++ py/matchFakeStars.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+"""
+matchFakes.py
+matches fakes based on position stored in the calibrated exposure image header
+"""
+
+import lsst.daf.persistence as dafPersist
+from lsst.afw.table.tableLib import SourceCatalog
+import numpy as np
+import argparse
+import re
+import collections
+
+def getFakeSources(rootdir, dataId, tol=0.1):
+    """Get list of sources which agree in position with fake ones with tol
+    """
+    butler = dafPersist.Butler(rootdir)
+
+    sources = butler.get('src', dataId)
+    cal_md  = butler.get('calexp_md', dataId)
+
+    # Get the X, Y locations of objects on the CCD
+    srcX, srcY = sources.getX(), sources.getY()
+    # Get the zeropoint
+    zeropoint = 2.5*np.log10(cal_md.get("FLUXMAG0"))
+    # Get the PSF flux and its error
+    flux, ferr = sources.getPsfFlux(), sources.getPsfFluxErr()
+    # Convert them into magnitude and its error
+    mag,  merr = 2.5*np.log10(flux), 2.5/np.log(10)*(ferr/flux)
+    mag = zeropoint - mag
+
+    # X, Y locations of the fake stars
+    fakeXY   = collections.defaultdict(tuple)
+    # Regular Expression
+    fakename = re.compile('FAKE([0-9]+)')
+    for card in cal_md.names():
+        m = fakename.match(card)
+        if m is not None:
+            x,y = map(float, (cal_md.get(card)).split(','))
+            fakeXY[int(m.group(1))] = (x,y)
+
+    srcIndex = collections.defaultdict(list)
+    for fid, fcoord  in fakeXY.items():
+        matched = ((np.abs(srcX-fcoord[0]) < tol) &
+                   (np.abs(srcY-fcoord[1]) < tol))
+        s1 = sources.subset(matched)
+        srcIndex[fid] = np.where(matched)[0]
+
+    srcList    = None
+    srcPsfMag  = []
+    srcPsfMerr = []
+    matchX     = []
+    matchY     = []
+    for s in srcIndex.values():
+        for ss in s:
+            if srcList is None:
+                srcList = SourceCatalog(sources.getSchema())
+            srcList.append(sources[ss])
+            #
+            srcPsfMag.append(mag[ss])
+            srcPsfMerr.append(merr[ss])
+            # Something is wrong here
+            matchX.append(srcX[ss])
+            matchY.append(srcY[ss])
+
+    return srcIndex, fakeXY, matchX, matchY, srcPsfMag, srcPsfMerr
+
+
+def main():
+
+    #TODO: this should use the LSST/HSC conventions
+    parser = argparse.ArgumentParser()
+    parser.add_argument('rootDir', help='root dir of data repo')
+    parser.add_argument('visit', help='id of visit', type=int)
+    parser.add_argument('ccd', help='id of ccd', type=int)
+    args = parser.parse_args()
+
+    #(starIndex,starList) = getFakeSources(args.rootDir, {'visit':args.visit, 'ccd':args.ccd})
+    (starIndex, fakeXY, matchX, matchY, starPsfMag, starPsfMerr) = getFakeSources(args.rootDir,
+                                                                                  {'visit':args.visit, 'ccd':args.ccd})
+    nMatch = len(fakeXY)
+    print "#  Number of Matched Stars : %d" % nMatch
+    print "#  Visit = %d   CCD = %d" % (args.visit, args.ccd)
+    print "#  FakeX    FakeY   PSFMag    PSFMagErr    Deblend "
+    for i in range(nMatch):
+       #print starIndex[i][0], starList[i]['flux.psf']
+       if len(starIndex[i]) > 1:
+           deblend = "blended"
+       else:
+           deblend = "isolate"
+       injectXY = fakeXY[i]
+
+       print "%d    %d    %f    %f    %s" % (injectXY[0], injectXY[1],
+                                             starPsfMag[i], starPsfMerr[i], deblend)
+
+
+if __name__=='__main__':
+    main()
