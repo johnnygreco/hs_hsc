76924c9d4fb2a424c98e1308001afc51e300bb4d
Song Huang <dr.guangtou@gmail.com>
2014-11-10 22:08:52 +0900

    typo fixed

--- py/fakes/matchFakeGalaxy.py
+++ py/fakes/matchFakeGalaxy.py
@@ -216,7 +216,7 @@ def save_to_ascii(params, root, visit, ccd):
 
     outTxt = rerun + '_' + str(visit).strip() + '_' + str(ccd).strip() + '_match.txt'
     f = open(outTxt, 'w')
-    header =  "# FakeX    FakeY    DiffX   DiffY   CmodMag   CmodErr  " + \
+    header =  "# Visit  CCD  FakeID  FakeX  FakeY   DiffX   DiffY   CmodMag   CmodErr  " + \
               "ExpMag  DevMag  KronMag  SdssR  SdssBa  SdssPa  " + \
               "ExpR  ExpBa  ExpPa  DevR  DevBa  DevPa  fracDev  parentID  nChild  extended\n"
     f.write(header)
