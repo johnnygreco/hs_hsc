c500f663f48345981e3cc57a49ea09df155634fa
dr-guangtou <dr.guangtou@gmail.com>
2015-02-21 22:29:07 +0900

    imagePrepare.py

--- py/imagePrepare.py
+++ py/imagePrepare.py
@@ -1,25 +1,118 @@
 #!/usr/bin/env python
 
 import argparse
-import numpy
-import matplotlib.pyplot as pyplot
-import lsst.daf.persistence as dafPersist
+import numpy                  as np
+import matplotlib.pyplot      as pyplot
+import lsst.daf.persistence   as dafPersist
+import lsst.afw.image         as afwImage
+from astropy.io import fits
 
-def main(rootDir, tract, patch, filter):
+def getPixelScale(wcs):
+
+    pixelScale = wcs.pixelScale().asArcseconds()
+    return pixelScale
+
+
+def getImgArr(mImg):
+
+    # Get the numpy ndarray for the image
+    imgArr = mImg.getImage().getArray()
+    # Get the numpy ndarray for the mask image
+    mskArr = mImg.getMask().getArray()
+    # Get the numpy ndarray for the variance
+    varArr = mImg.getVariance().getArray()
+
+    return imgArr, mskArr, varArr
+
+def getBkgArr(bImg):
+
+    bkgArr = bImg.getImage().getArray()
+    return bkgArr
+
+def savePsfArr(calExp, psfFile):
+
+    psfImg = calExp.getPsf().computeImage()
+    psfImg.writeFits(psfFile)
+    return psfImg.getArray()
+
+def getZeroPoint(butler, dataId):
+
+    calMeta = butler.get('deepCoadd_md', dataId, immediate=True)
+    zeropoint = (2.5 * np.log10(calMeta.get("FLUXMAG0")))
+    return zeropoint
+
+
+def imagePrepare(rootDir, tract, patch, filt, prefix):
 
     # Make a butler and specify the dataId
     butler = dafPersist.Butler(rootDir)
-    dataId = {'tract': tract, 'patch': patch, 'filter': filter}
+    dataId = {'tract': tract, 'patch': patch, 'filter': filt}
+
+    # Get the prefix of the output files
+    prefix = prefix + '-' + str(tract) + '-' + patch + '-' + filt
 
     # Get the exposure from the butler
-    exposure = butler.get('deepCoadd_calexp')
+    calExp = butler.get('deepCoadd', dataId, immediate=True)
+    # Get the WCS information
+    wcs = calExp.getWcs()
+    # Get the pixel scale of the image
+    pixelScale = getPixelScale(wcs)
+
+    # Get the flux zeropoint
+    zeropoint = getZeroPoint(butler, dataId)
+
+    # Get the maskedImageObject
+    mImg = calExp.getMaskedImage()
+    imgArr, mskArr, varArr = getImgArr(mImg)
+    # Turns the variance array into sigma array
+    sigArr = np.sqrt(varArr)
+
+    # Get the numpy ndarray for the background
+    bImg = butler.get('deepCoadd_calexpBackground', dataId, immediate=True)
+    bkgArr = getBkgArr(bImg)
+
+    # Add the background back on to the image
+    oriArr = (imgArr + bkgArr)
+
+    # Get the PSF array
+    psfFile = prefix + '_psf.fits'
+    psfArr = savePsfArr(calExp, psfFile)
+
+    # Get the header of the images
+    fitsName = butler.get('deepCoadd_filename', dataId)
+    hduList = fits.open(fitsName)
+    imgHeader = hduList[1].header
+    mskHeader = hduList[2].header
+    varHeader = hduList[3].header
+
+    # Update the header with more information
+    imgHeader.set('PIXELSCALE', pixelScale)
+    imgHeader.set('ZEROPOINT',  zeropoint)
+
+    # Write the Image array
+    imgFile = prefix + '_img.fits'
+    fits.writeto(imgFile, imgArr, imgHeader)
+    # Write the Mask array
+    mskFile = prefix + '_msk.fits'
+    fits.writeto(mskFile, mskArr, mskHeader)
+    # Write the Sigma array
+    sigFile = prefix + '_sig.fits'
+    fits.writeto(sigFile, sigArr, varHeader)
+    # Write the Image + Background array
+    oriFile = prefix + '_ori.fits'
+    fits.writeto(oriFile, oriArr, imgHeader)
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("root", help="Root directory of data repository")
-    parser.add_argument("tract", type=int, help="Visit to show")
-    parser.add_argument("patch",  help="CCD to show")
-    parser.add_argument("filter", help="Filter")
+    parser.add_argument("tract", type=int, help="Tract to show")
+    parser.add_argument("patch",  help="Patch to show")
+    parser.add_argument('-f', '--filter', dest='filt', help="Filter",
+                       default='HSC-I')
+    parser.add_argument('-p', '--prefix', dest='outfile',
+                        help='Prefix of the output file', default='hsc')
     args = parser.parse_args()
 
-    main(args.root, args.tract, args.patch, args.filter)
+    imagePrepare(args.root, args.tract, args.patch, args.filt, args.outfile)
--- py/srcCatPrepare.py
+++ py/srcCatPrepare.py
@@ -1,25 +1,86 @@
 #!/usr/bin/env python
+"""
+srcCatPrepare.py
+Generate a copy of the src-HSC catalog that is easier to read
+"""
 
-import argparse
-import numpy
-import matplotlib.pyplot as pyplot
 import lsst.daf.persistence as dafPersist
+from lsst.afw.table import SourceCatalog, SchemaMapper
+import lsst.afw.geom
+import lsst.pex.exceptions
+import numpy as np
+import argparse
+import re
+import collections
+import astropy.table
+import lsst.afw.geom.ellipses
 
-def main(rootDir, tract, patch, filter):
+def getMag(flux, fluxerr, zeropoint):
+    """
+    Return the magnitude and error
+    """
+    mag, magerr = -2.5 * np.log10(flux), 2.5/np.log(10.0)*fluxerr/flux
+    return (mag.T + zeropoint).T, magerr
+
+def getMagBatch(calExp, fluxArr, ferrArr=None):
+    """
+    Return the magnitude and error for a list of flux
+    """
+    calCalib = calExp.getCalib()
+    calCalib.setThrowOnNegativeFlux(False)
+    if ferrArr is None:
+        magArr = calCalib.getMagnitude(fluxArr)
+        return magArr
+    else:
+        magArr, merrArr = calCalib.getMagnitude(fluxArr, ferrArr)
+        return magArr, merrArr
+
+def getS2N(fluxArr, ferrArr):
+    """
+    Get the S/N of the detections
+    TODO: Is this the right way
+    """
+    return (fluxArr / ferrArr)
+
+def getEllipse(quad):
+    """
+    Returns the semi-major axis, axes ratio and PA for a given quadrupole moment
+    """
+    e = lsst.afw.geom.ellipses.Axes(quad)
+    return e.getA(), e.getB()/e.getA(), e.getTheta() * 180.0/np.pi
+
+
+def getSrcData(butler, dataId):
+    """
+    Get the src catalog, exposure, metadata from the butler
+    """
+    srcCat = butler.get('deepCoadd_src', dataId, immediate=True,
+                       flags=lsst.afw.table.SOURCE_IO_NO_FOOTPRINTS)
+    calExp = butler.get('deepCoadd',    dataId)
+    calMd  = butler.get('deepCoadd_md', dataId)
+    return srcCat, calExp, calMd
+
+
+def srcCatPrepare(rootDir, tract, patch, filt):
 
     # Make a butler and specify the dataId
     butler = dafPersist.Butler(rootDir)
-    dataId = {'tract': tract, 'patch': patch, 'filter': filter}
+    dataId = {'tract': tract, 'patch': patch, 'filter': filt}
+    # Return the src catalog, exposure, and the metadata
+    srcCat, calExp, calMd = getSrcData(butler, dataId)
 
-    # Get the exposure from the butler
-    exposure = butler.get('deepCoadd_calexp')
+    # Get the WCS and pixel scale
+
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("root", help="Root directory of data repository")
-    parser.add_argument("tract", type=int, help="Visit to show")
-    parser.add_argument("patch",  help="CCD to show")
-    parser.add_argument("filter", help="Filter")
+    parser.add_argument('root',  help="Root directory of data repository")
+    parser.add_argument('tract', type=int, help="Visit to show")
+    parser.add_argument('patch', help="patch to show")
+    parser.add_argument('-f', '--filter', dest='filt', help="Filter",
+                       default='HSC-I')
     args = parser.parse_args()
 
-    main(args.root, args.tract, args.patch, args.filter)
+    srcCatPrepare(args.root, args.tract, args.patch, args.filt)
