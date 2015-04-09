0d396dbe484074effc45f83b8c4385b92a179d1f
dr.guangtou <dr.guangtou@gmail.com>
2015-04-09 17:49:55 +0900

    Directly search for FITS images

--- py/coaddImgCornerRaDec.py
+++ py/coaddImgCornerRaDec.py
@@ -6,6 +6,18 @@ import argparse
 import numpy
 import os
 
+def listAllImages(rootDir, filter):
+
+    import glob
+
+    if rootDir[-1] is '/':
+        searchDir = rootDir + 'deepCoadd/' + filter.upper() + '/*/*.fits'
+    else:
+        searchDir = rootDir + '/deepCoadd/' + filter.upper() + '/*/*.fits'
+
+    return map(lambda x: x, glob.glob(searchDir))
+
+
 def imgCornersRaDec(fileName, hdu=0, output=False):
 
     if not os.path.isfile(fileName):
@@ -44,86 +56,99 @@ def imgCornersRaDec(fileName, hdu=0, output=False):
 
     return raDec
 
-def coaddImgCornersRaDec(root, listName):
+def coaddImgCornersRaDec(root, filt, prefix=None):
 
-    if not os.path.isfile(listName):
-        raise Exception('Can not find the list file: %s' % listName)
-    else:
-        imgList = open(listName, 'r')
+    # Get the image list
+    imgList = listAllImages(root, filt)
 
-        # Define the output FITS name
-        outFits = os.path.splitext(listName)[0] + '_corners.fits'
+    # Define the output FITS name
+    if prefix is None:
+        prefix = os.path.split(root)[-1].lower()
+    outFits = prefix + '_' + filt + '_corners.fits'
 
-        # Define all the columns
-        files, rerun, filter, tract, patch = [], [], [], [], []
-        raLL, decLL, raUL, decUL = [], [], [], []
-        raLR, decLR, raUR, decUR = [], [], [], []
+    # Define all the columns
+    files, rerun, filter, tract, patch = [], [], [], [], []
+    raLL, decLL, raUL, decUL = [], [], [], []
+    raLR, decLR, raUR, decUR = [], [], [], []
 
-        for img in imgList.readlines():
-            # Get rid of the '\n' at the end of the line
-            imgFile = root + '/' + img.strip()
-            files.append(imgFile)
+    for img in imgList.readlines():
+        # Get rid of the '\n' at the end of the line
+        imgFile = root + '/' + img.strip()
+        files.append(imgFile)
 
-            segFile = imgFile.split('/')
-            if len(segFile) >= 5:
-                rerun.append(segFile[-5])
-                filter.append(segFile[-3])
-                tract.append(int(segFile[-2]))
-                patch.append(segFile[-1].split('.')[0])
-            else:
-                raise Exception("Image location is not correct!")
+        segFile = imgFile.split('/')
+        if len(segFile) >= 5:
+            rerun.append(segFile[-5])
+            filter.append(segFile[-3])
+            tract.append(int(segFile[-2]))
+            patch.append(segFile[-1].split('.')[0])
+        else:
+            raise Exception("Image location is not correct!")
 
-            # Get the corner coordinates
-            imgRaDec = imgCornersRaDec(imgFile, hdu=1)
-            # Lower left
-            raLL.append(imgRaDec[0][0])
-            decLL.append(imgRaDec[0][1])
-            # Upper left
-            raUL.append(imgRaDec[1][0])
-            decUL.append(imgRaDec[1][1])
-            # Lower right
-            raLR.append(imgRaDec[2][0])
-            decLR.append(imgRaDec[2][1])
-            # Upper right
-            raUR.append(imgRaDec[3][0])
-            decUR.append(imgRaDec[3][1])
+        # Get the corner coordinates
+        imgRaDec = imgCornersRaDec(imgFile, hdu=1)
+        # Lower left
+        raLL.append(imgRaDec[0][0])
+        decLL.append(imgRaDec[0][1])
+        # Upper left
+        raUL.append(imgRaDec[1][0])
+        decUL.append(imgRaDec[1][1])
+        # Lower right
+        raLR.append(imgRaDec[2][0])
+        decLR.append(imgRaDec[2][1])
+        # Upper right
+        raUR.append(imgRaDec[3][0])
+        decUR.append(imgRaDec[3][1])
 
-        columns = fits.ColDefs([
-            fits.Column(name='file',  format='90A', array=numpy.array(files)),
-            fits.Column(name='rerun', format='20A', array=numpy.array(rerun)),
-            fits.Column(name='filter',format='5A', array=numpy.array(filter)),
-            fits.Column(name='tract', format='I', array=numpy.array(tract)),
-            fits.Column(name='patch', format='3A', array=numpy.array(patch)),
-            fits.Column(name='raLL',  format='E', array=numpy.array(raLL),
-                        unit='degree'),
-            fits.Column(name='decLL', format='E', array=numpy.array(decLL),
-                        unit='degree'),
-            fits.Column(name='raUL',  format='E', array=numpy.array(raUL),
-                        unit='degree'),
-            fits.Column(name='decUL', format='E', array=numpy.array(decUL),
-                        unit='degree'),
-            fits.Column(name='raLR',  format='E', array=numpy.array(raLR),
-                        unit='degree'),
-            fits.Column(name='decLR', format='E', array=numpy.array(decLR),
-                        unit='degree'),
-            fits.Column(name='raUR',  format='E', array=numpy.array(raUR),
-                        unit='degree'),
-            fits.Column(name='decUR', format='E', array=numpy.array(decUR),
-                        unit='degree')])
+    columns = fits.ColDefs([
+        fits.Column(name='file',  format='90A', array=numpy.array(files)),
+        fits.Column(name='rerun', format='20A', array=numpy.array(rerun)),
+        fits.Column(name='filter',format='5A', array=numpy.array(filter)),
+        fits.Column(name='tract', format='I', array=numpy.array(tract)),
+        fits.Column(name='patch', format='3A', array=numpy.array(patch)),
+        fits.Column(name='raLL',  format='E', array=numpy.array(raLL),
+                    unit='degree'),
+        fits.Column(name='decLL', format='E', array=numpy.array(decLL),
+                    unit='degree'),
+        fits.Column(name='raUL',  format='E', array=numpy.array(raUL),
+                    unit='degree'),
+        fits.Column(name='decUL', format='E', array=numpy.array(decUL),
+                    unit='degree'),
+        fits.Column(name='raLR',  format='E', array=numpy.array(raLR),
+                    unit='degree'),
+        fits.Column(name='decLR', format='E', array=numpy.array(decLR),
+                    unit='degree'),
+        fits.Column(name='raUR',  format='E', array=numpy.array(raUR),
+                    unit='degree'),
+        fits.Column(name='decUR', format='E', array=numpy.array(decUR),
+                    unit='degree')])
 
-        tabHdu = fits.TableHDU.from_columns(columns)
-        priHdu = fits.PrimaryHDU([0])
-        hduList = fits.HDUList([priHdu, tabHdu])
-        hduList.writeto(outFits, clobber=True)
+    tabHdu = fits.TableHDU.from_columns(columns)
+    priHdu = fits.PrimaryHDU([0])
+    hduList = fits.HDUList([priHdu, tabHdu])
+    hduList.writeto(outFits, clobber=True)
 
-        return columns
+    return columns
+
+def coaddAllBandsCorners(root, prefix=None):
+
+    hscFilters = ['HSC-G', 'HSC-R', 'HSC-I', 'HSC-Z', 'HSC-Y']
+
+    multiTable = []
+    for filt in hscFilters:
+        cornerTable = coaddImgCornersRaDec(root, filt, prefix=None)
+        multiTable.append(cornerTable)
+
+    return multiTable
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("root", help="Root directory of data repository")
-    parser.add_argument("name", help="Name of the image list file")
+    parser.add_argument("filt", help="HSC filter")
+    parser.add_argument('-p', '--prefix', dest='prefix',
+                        help='Prefix of the output file',
+                        default=None)
     args = parser.parse_args()
 
-    coaddImgCornersRaDec(args.root, args.name)
-
+    coaddImgCornersRaDec(args.root, args.filt, prefix=args.prefix)
