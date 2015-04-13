6dd0274ee29f94a33e0b81c4351e6600df6e814a
dr-guangtou <dr.guangtou@gmail.com>
2015-04-13 14:31:49 +0900

    redudent

--- py/coscoaddExamples.py
+++
@@ -1,6 +0,0 @@
-
-
-# Get the corner (Ra,Dec) for a list of images 
-
-from coaddImgCornerRaDec import *
-multi = coaddAllBandsCorners('/lustre/Subaru/SSP/rerun/yasuda/SSP3.4.1_20141224_cosmos/', prefix='ssp341_cosmos')
