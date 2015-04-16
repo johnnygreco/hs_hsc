3f3d70cc4a7a5ed21450adce1d15395ce258c3a2
dr.guangtou <dr.guangtou@gmail.com>
2015-04-16 17:15:25 +0900

    hscsUtil and galfitParser code

---
+++ py/galfitParser.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+# Extract information from GALFIT output FITS file
+#
+# This is based on astronomeralex's galfit-python-parser:
+#    https://github.com/astronomeralex/galfit-python-parser
+# Modified by Song Huang to include more features
+#
+
+from astropy.io import fits
+
+class GalfitComponent(object):
+    """
+    stores results from one component of the fit
+    """
+    def __init__(self,galfitheader,component_number):
+        """
+        takes in the fits header from HDU 3 (the galfit model) from a galfit output file
+        and the component number to extract
+        """
+        #checks
+        assert component_number > 0
+        assert "COMP_" + str(component_number) in galfitheader
+
+        self.component_type = galfitheader["COMP_" + str(component_number)]
+        self.component_number = component_number
+        headerkeys = [i for i in galfitheader.keys()]
+        comp_params = []
+        for i in headerkeys:
+            if str(component_number) + '_' in i:
+                comp_params.append(i)
+        for param in comp_params:
+            val = galfitheader[param]
+            #we know that val is a string formatted as 'result +/- uncertainty'
+            val = val.split()
+            paramsplit = param.split('_')
+            setattr(self,paramsplit[1].lower(),float(val[0]))
+            setattr(self,paramsplit[1].lower() + '_err',float(val[2]))
+
+
+
+class GalfitResults(object):
+    """
+    This class stores galfit results information
+    Currently only does one component
+    """
+    def __init__(self, galfit_fits_file):
+        """
+        init method for GalfitResults. Take in a string that is the name
+        of the galfit output fits file
+        """
+        hdulist = fits.open(galfit_fits_file)
+        #now some checks to make sure the file is what we are expecting
+        assert len(hdulist) == 4
+        galfitmodel = hdulist[2]
+        galfitheader = galfitmodel.header
+        galfit_in_comments = False
+        for i in galfitheader['COMMENT']:
+            galfit_in_comments = galfit_in_comments or "GALFIT" in i
+        assert True == galfit_in_comments
+        assert "COMP_1" in galfitheader
+        #now we've convinced ourselves that this is probably a galfit file
+
+        self.galfit_fits_file = galfit_fits_file
+        #read in the input parameters
+        self.input_initfile = galfitheader['INITFILE']
+        self.input_datain = galfitheader["DATAIN"]
+        self.input_sigma = galfitheader["SIGMA"]
+        self.input_psf = galfitheader["PSF"]
+        self.input_constrnt = galfitheader["CONSTRNT"]
+        self.input_mask = galfitheader["MASK"]
+        self.input_fitsect = galfitheader["FITSECT"]
+        self.input_convbox = galfitheader["CONVBOX"]
+        self.input_magzpt = galfitheader["MAGZPT"]
+
+        #read in the chi-square value
+        self.chisq = galfitheader["CHISQ"]
+        self.ndof = galfitheader["NDOF"]
+        self.nfree = galfitheader["NFREE"]
+        self.reduced_chisq = galfitheader["CHI2NU"]
+
+        #find the number of components
+        num_components = 1 #already verified above
+        while True:
+            if "COMP_" + str(num_components + 1) in galfitheader:
+                num_components = num_components + 1
+            else:
+                break
+        self.num_components = num_components
+
+        for i in range(1, self.num_components + 1):
+            setattr(self,"component_" + str(i),GalfitComponent(galfitheader,i))
+
+        hdulist.close()
+
---
+++ py/hscsUtil.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+
+"""
+Collection of useful tools to deal with galaxy images from HSC and other surveys
+
+ * Cosmology related procedures
+ * Galactic extinction related
+ * Angle normalization
+ * Coordinate related shortcuts
+
+"""
+
+import numpy as np
+from astropy.utils.misc import isiterable
+
+"""
+Angle related functions:
+
+Strongly based on: https://github.com/phn/angles/blob/master/angles.py
+by Prasanth Nair
+
+"""
+
+def rad2deg(rad):
+    """ Convert radians into degrees"""
+    return (rad * 180.0 / np.pi)
+
+def deg2rad(deg):
+    """ Convert degrees into radians"""
+    return (deg * np.pi / 180.0)
+
+def hr2deg(deg):
+    """ Convert degrees into hours"""
+    return (deg *(24.0 / 360.0))
+
+def deg2hr(hr):
+    """ Convert hours into degrees"""
+    return (hr * 15.0)
+
+def normalize(num, lower=0, upper=360, b=False):
+    """Normalize number to range [lower, upper) or [lower, upper].
+    Parameters
+    ----------
+    num : float
+        The number to be normalized.
+    lower : int
+        Lower limit of range. Default is 0.
+    upper : int
+        Upper limit of range. Default is 360.
+    b : bool
+        Type of normalization. Default is False. See notes.
+    Returns
+    -------
+    n : float
+        A number in the range [lower, upper) or [lower, upper].
+    Raises
+    ------
+    ValueError
+      If lower >= upper.
+    Notes
+    -----
+    If the keyword `b == False`, then the normalization is done in the
+    following way. Consider the numbers to be arranged in a circle,
+    with the lower and upper ends sitting on top of each other. Moving
+    past one limit, takes the number into the beginning of the other
+    end. For example, if range is [0 - 360), then 361 becomes 1 and 360
+    becomes 0. Negative numbers move from higher to lower numbers. So,
+    -1 normalized to [0 - 360) becomes 359.
+    If the keyword `b == True`, then the given number is considered to
+    "bounce" between the two limits. So, -91 normalized to [-90, 90],
+    becomes -89, instead of 89. In this case the range is [lower,
+    upper]. This code is based on the function `fmt_delta` of `TPM`.
+    Range must be symmetric about 0 or lower == 0.
+    Examples
+    --------
+    >>> normalize(-270,-180,180)
+    90.0
+    >>> import math
+    >>> math.degrees(normalize(-2*math.pi,-math.pi,math.pi))
+    0.0
+    >>> normalize(-180, -180, 180)
+    -180.0
+    >>> normalize(180, -180, 180)
+    -180.0
+    >>> normalize(180, -180, 180, b=True)
+    180.0
+    >>> normalize(181,-180,180)
+    -179.0
+    >>> normalize(181, -180, 180, b=True)
+    179.0
+    >>> normalize(-180,0,360)
+    180.0
+    >>> normalize(36,0,24)
+    12.0
+    >>> normalize(368.5,-180,180)
+    8.5
+    >>> normalize(-100, -90, 90)
+    80.0
+    >>> normalize(-100, -90, 90, b=True)
+    -80.0
+    >>> normalize(100, -90, 90, b=True)
+    80.0
+    >>> normalize(181, -90, 90, b=True)
+    -1.0
+    >>> normalize(270, -90, 90, b=True)
+    -90.0
+    >>> normalize(271, -90, 90, b=True)
+    -89.0
+    """
+    from math import floor, ceil
+    # abs(num + upper) and abs(num - lower) are needed, instead of
+    # abs(num), since the lower and upper limits need not be 0. We need
+    # to add half size of the range, so that the final result is lower +
+    # <value> or upper - <value>, respectively.
+    res = num
+    if not b:
+        if lower >= upper:
+            raise ValueError("Invalid lower and upper limits: (%s, %s)" %
+                             (lower, upper))
+
+        res = num
+        if num > upper or num == lower:
+            num = lower + abs(num + upper) % (abs(lower) + abs(upper))
+        if num < lower or num == upper:
+            num = upper - abs(num - lower) % (abs(lower) + abs(upper))
+
+        res = lower if num == upper else num
+    else:
+        total_length = abs(lower) + abs(upper)
+        if num < -total_length:
+            num += ceil(num / (-2 * total_length)) * 2 * total_length
+        if num > total_length:
+            num -= floor(num / (2 * total_length)) * 2 * total_length
+        if num > upper:
+            num = total_length - num
+        if num < lower:
+            num = -total_length - num
+
+        res = num
+
+    res *= 1.0  # Make all numbers float, to be consistent
+
+    return res
+
+
+"""
+Coordinate related shortcuts
+
+    * Convert from (ra, dec) to (l, b)
+"""
+
+def radec2lb(ra, dec):
+
+    """
+    Convert (ra, dec) into Galactic coordinate (l, b)
+
+    Parameters
+    ----------
+    ra : float or list or array
+        RA Coordinates in degree
+    dec : float or list or array
+        DEC Coordinates in degree
+
+    Returns
+    -------
+    l : float or list or array
+    b : float or list or array
+    """
+
+    """ See if
+    if not (isiterable(ra) or isiterable(dec)):
+
+
