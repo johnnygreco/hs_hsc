c8144535428cf4b388356ce5dd85e0d108a66eb4
Song Huang <dr.guangtou@gmail.com>
2015-04-22 20:08:49 +0900

    Padding the image; Extra information; Scale bar; Local max

--- py/hscCoadd/coaddColourImage.py
+++ py/hscCoadd/coaddColourImage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import division
+
 import os
 import argparse
 import numpy   as np
@@ -32,7 +34,9 @@ def getCircleRaDec(ra, dec, size):
     return raList, decList
 
 
-def saveRgbPng(outRgb, imgRgb, xCen=None, yCen=None, name=None, info=None):
+def saveRgbPng(outRgb, imgRgb, cenMark=False, xCen=None, yCen=None, name=None,
+               info1=None, info2=None, info3=None, sLength=None,
+               sString=None):
 
     """
     Save the RGB image as a PNG figure
@@ -54,8 +58,8 @@ def saveRgbPng(outRgb, imgRgb, xCen=None, yCen=None, name=None, info=None):
     ax.yaxis.set_visible(False)
 
     # Highlight the new center: TODO
-    if (xCen is not None) and (yCen is not None):
-        ax.scatter(xCen, yCen, s=60, lw=2.0, marker='o', edgecolors='r',
+    if cenMark and (xCen is not None) and (yCen is not None):
+        ax.scatter(xCen, yCen, s=80, lw=0.5, marker='o', edgecolors='r',
                    facecolors='none')
 
     # Add some information on the image
@@ -63,10 +67,25 @@ def saveRgbPng(outRgb, imgRgb, xCen=None, yCen=None, name=None, info=None):
         ax.text(0.5, 0.10, name, fontsize=20, fontweight='bold',
                 ha='center', va='center', color='w',
                 transform=ax.transAxes)
-    if info is not None:
-        ax.text(0.8, 0.88, info, fontsize=18, fontweight='bold',
+    if info1 is not None:
+        ax.text(0.8, 0.90, info1, fontsize=18, fontweight='bold',
                 ha='center', va='center', color='w',
                 transform=ax.transAxes)
+    if info2 is not None:
+        ax.text(0.8, 0.82, info2, fontsize=18, fontweight='bold',
+                ha='center', va='center', color='w',
+                transform=ax.transAxes)
+    if info3 is not None:
+        ax.text(0.8, 0.74, info3, fontsize=18, fontweight='bold',
+                ha='center', va='center', color='w',
+                transform=ax.transAxes)
+        if sLength is not None:
+            ax.plot([0.14, 0.14+sLength], [0.88, 0.88], 'w-', lw=2.5,
+                    transform=ax.transAxes)
+            if sString is not None:
+                ax.text((0.28 + sLength)/2.0, 0.85, sString, fontsize=15,
+                        ha='center', va='center', color='w',
+                        fontweight='bold', transform=ax.transAxes)
 
     ax.margins(0.00, 0.00, tight=True)
 
@@ -85,8 +104,9 @@ def isHscFilter(filter, short=True):
 
 
 def coaddColourImage(root, ra, dec, size, filt='gri',
-                     prefix='hsc_coadd_cutout', info=None,
-                     min=-0.0, max=0.70, Q=10, name=None):
+                     prefix='hsc_coadd_cutout', info1=None, info2=None, info3=None,
+                     min=-0.0, max=0.70, Q=10, name=None, localMax=True,
+                     scaleBar=10):
 
     # Get the SkyMap of the database
     butler = dafPersist.Butler(root)
@@ -98,6 +118,9 @@ def coaddColourImage(root, ra, dec, size, filt='gri',
     # Expected size and center position
     dimExpect = (2 * size +1)
     cenExpect = (dimExpect/2.0, dimExpect/2.0)
+    # Create a empty array
+    # For RGB image, the data type should be uint8
+    rgbEmpty = np.empty((dimExpect, dimExpect, 3), dtype="uint8")
 
     # Check the choice of filters
     if len(filt) is not 3:
@@ -142,9 +165,9 @@ def coaddColourImage(root, ra, dec, size, filt='gri',
 
         #Then we can read the desired pixels
         images = {}
-        newcen = {}
-        height = {}
-        width  = {}
+        #newcen = {}
+        newX   = {}
+        newY   = {}
         cutoutSize = int(size)
 
         for i in range(3):
@@ -165,50 +188,90 @@ def coaddColourImage(root, ra, dec, size, filt='gri',
 
             # Grow the bounding box to the desired size
             bbox.grow(int(cutoutSize))
+            xOri, yOri = bbox.getBegin()
 
             # Compare to the coadd image, and clip
             bbox.clip(coadd.getBBox(afwImage.PARENT))
+            newX[i] = bbox.getBeginX() - xOri
+            newY[i] = bbox.getBeginY() - yOri
 
             # Get the masked image
             subImage  = afwImage.ExposureF(coadd, bbox, afwImage.PARENT)
             # Get the WCS
-            subWcs = subImage.getWcs()
-
+            #subWcs = subImage.getWcs()
             # Get the central pixel coordinates on new subImage WCS
-            newcen[i] = subWcs.skyToPixel(raDec)
+            #newcen[i] = subWcs.skyToPixel(raDec)
 
-            height[i] = bbox.getHeight()
-            width[i]  = bbox.getWidth()
-
-            images[i] = subImage.getMaskedImage()
+            # Extract the image array
+            images[i] = subImage.getMaskedImage().getImage()
 
         # Define the Blue, Green, and Red channels
-        B, G, R = images[0].getImage(), images[1].getImage(), images[2].getImage()
+        # These cutouts are still HSC ImageF object, not numpy array
+        bCut, gCut, rCut = images[0], images[1], images[2]
 
-        for j in range(3):
-            newCenX, newCenY = newcen[j]
-            newOriX, newOriY = images[j].getImage().getXY0()
-            newCenX = newCenX - newOriX
-            newCenY = height[j] - (newCenY - newOriY)
-        newX = newCenX
-        newY = newCenY
+        if localMax:
+            maxArr = []
+            for m in range(3):
+                imgPad = np.empty((dimExpect, dimExpect), dtype=float)
+                imgPad[newY[m]:(newY[m] + images[m].getHeight()),
+                       newX[m]:(newX[m] + images[m].getWidth())] = images[m].getArray()
+                globalMax = np.max(images[m].getArray())
+                localMax = np.max(imgPad[cenExpect[0]-10:cenExpect[0]+10,
+                                  cenExpect[1]-10:cenExpect[1]+10])
+                maxArr.append(localMax / globalMax)
+                #print "### %d : %6.3f" % (m+1, localMax/globalMax)
+            maxShow = np.max(np.asarray(maxArr))
+        else:
+            maxShow = max
+
+        #for j in range(3):
+            #newCenX, newCenY = newcen[j]
+            #newOriX, newOriY = images[j].getXY0()
+            #newCenX = newCenX - newOriX
+            #newCenY = images[j].getHeight() - (newCenY - newOriY)
 
         # To see if data are available for all the cut-out region
-        if (B.getHeight < dimExpect) or (B.getWidth() < dimExpect):
+        if (bCut.getHeight() < dimExpect) or (bCut.getWidth() < dimExpect):
             print " ### Only part of the desired cutout-region is returned !"
             # Define the name of the output file
             outRgb = prefix + '_' + filt + '_part_color.png'
+            partial = True
         else:
             outRgb = prefix + '_' + filt + '_color.png'
+            partial = False
 
         # Generate the RGB image
-        imgRgb = afwRgb.makeRGB(R, G, B, min=min, range=(max - min), Q=Q,
+        # 15/04/22: min ==> minimum
+        imgRgb = afwRgb.makeRGB(rCut, gCut, bCut, minimum=min,
+                               range=(maxShow - min), Q=Q,
                                saturatedPixelValue=None)
+        if partial:
+            for k in range(3):
+                rgbEmpty[newY[k]:(newY[k] + images[k].getHeight()),
+                        newX[k]:(newX[k] + images[k].getWidth()), k] = imgRgb[:, :, k]
+            imgRgb = rgbEmpty
+
+        # Add a scale bar
+        if scaleBar is not None:
+            sLength = (scaleBar * 1.0) / 0.170 / (dimExpect * 1.0)
+            print "### %10.3f" % sLength
+            sString = "%d\"" % int(scaleBar)
+        else:
+            sLength = None
+            sString = None
 
         # Better way to show the image
-        saveRgbPng(outRgb, imgRgb, xCen=newX, yCen=newY, name=name, info=info)
+        if partial:
+            saveRgbPng(outRgb, imgRgb, cenMark=True, xCen=cenExpect[0],
+                       yCen=cenExpect[1], sLength=sLength, sString=sString,
+                       name=name, info1=info1, info2=info2, info3=info3)
+        else:
+            saveRgbPng(outRgb, imgRgb,  name=name,
+                       info1=info1, info2=info2, info3=info3,
+                       sLength=sLength, sString=sString)
 
     #return imgRgb
+
 
 def getTractPatchList(matches):
 
@@ -224,6 +287,7 @@ def getTractPatchList(matches):
 
     return tract, patch
 
+
 def getFitsImgName(root, tract, patch, filter, imgType='deepCoadd'):
 
     if root[-1] is not '/':
@@ -232,106 +296,6 @@ def getFitsImgName(root, tract, patch, filter, imgType='deepCoadd'):
     imgName = root + imgType + '/' + filter + '/' + str(tract) + '/' + patch + '.fits'
 
     return imgName
-
-
-def coaddColourImageFull(root, ra, dec, size, filt='gri',
-                        prefix='hsc_coadd_cutout', info=None,
-                        min=-0.0, max=0.6, Q=4):
-
-    # Get the SkyMap of the database
-    butler = dafPersist.Butler(root)
-    skyMap = butler.get("deepCoadd_skyMap", immediate=True)
-
-    # [Ra, Dec] pair
-    raDec = afwCoord.Coord(ra*afwGeom.degrees, dec*afwGeom.degrees)
-    # [Ra, Dec] List
-    raList, decList = getCircleRaDec(ra, dec, size*0.8)
-    points = map(lambda x, y: afwGeom.Point2D(x, y), raList, decList)
-    raDec  = map(lambda x: afwCoord.IcrsCoord(x), points)
-
-    # Check the choice of filters
-    if len(filt) is not 3:
-        raise Exception("Have to be three filters!")
-    elif not (isHscFilter(filt[0]) & isHscFilter(filt[1]) &
-              isHscFilter(filt[2])):
-        raise Exception("Not all filters are valid !")
-    # Get the correct HSC filter name
-    filter1 = "HSC-%s" % filt[0].upper()
-    filter2 = "HSC-%s" % filt[1].upper()
-    filter3 = "HSC-%s" % filt[2].upper()
-    filtArr = [filter1, filter2, filter3]
-
-    cutoutSize = int(size)
-
-    #Figure out the area we want, and read the data.
-    #For coadds the WCS is the same in all bands, but the code handles the general case
-    #Start by finding the tract and patch
-    #tractInfo, patchInfo = skyMap.findClosestTractPatchList(raDec)
-    matches = skyMap.findClosestTractPatchList(raDec)
-    # Return a list of tracts and patches
-    tracts, patches = getTractPatchList(matches)
-    # Number of unique tracts (?)
-    nTract = len(matches)
-    nPatch = len(patches)
-    print "### Match returns %d tract(s) and %d patch(es) !" % (nTract, nPatch)
-
-    for tract, patch in zip(tracts, patches):
-
-        for filter in filtArr:
-            fitsName = getFitsImgName(root, tract, patch, filter,
-                                      imgType='deepCoadd')
-            if not os.path.isfile():
-                print " XXX Can not find image: %s" % fitsName
-            else:
-                coadd = butler.get("deepCoadd", immediate=True,
-                                   tract=tract, patch=patch, filter=filter)
-                # Get the WCS information
-                wcs = coadd.getWcs()
-                # Convert the central coordinate from Ra,Dec to pixel unit
-                pixel = wcs.skyToPixel(raDec)
-                pixel = afwGeom.Point2I(pixel)
-                # Define the bounding box for the central pixel
-                bbox = afwGeom.Box2I(pixel, pixel)
-                # Grow the bounding box to the desired size
-                bbox.grow(int(cutoutSize))
-                bbox.clip(coadd.getBBox(afwImage.PARENT))
-                # Get the masked image
-                subImage  = afwImage.ExposureF(coadd, bbox, afwImage.PARENT)
-                # Get the WCS
-                subWcs = subImage.getWcs()
-
-        ## TODO: To be continued ...
-            images[i] = subImage.getMaskedImage()
-            wcs[i] = subWcs
-
-        # Define the Blue, Green, and Red channels
-        # TODO: Need to be tested
-        B, G, R = images[0].getImage(), images[1].getImage(), images[2].getImage()
-        cenB, cenG, cenR = wcs[0].skyToPixel(raDec), wcs[1].skyToPixel(raDec), \
-                wcs[2].skyToPixel(raDec)
-
-        # Get the image coordinate for the input (RA, DEC)
-        # TODO
-
-        # To see if data are available for all the cut-out region
-        dimExpect = (2 * size +1)
-        if (B.getHeight < dimExpect) or (B.getWidth() < dimExpect):
-            print " ### Only part of the desired cutout-region is returned !"
-            # Define the name of the output file
-            outRgb = prefix + '_' + filt + '_part_color.png'
-        else:
-            outRgb = prefix + '_' + filt + '_color.png'
-
-        # Generate the RGB image
-        imgRgb = afwRgb.makeRGB(R, G, B, min=min, range=(max - min), Q=Q,
-                               saturatedPixelValue=None)
-
-        # Better way to show the image
-        # TODO
-        saveRgbPng(outRgb, imgRgb)
-        # afwRgb.writeRGB(outRgb, imgRgb)
-
-    return imgRgb
 
 
 if __name__ == '__main__':
