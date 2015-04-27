e839f4cca644795e8af361c75674b85b0d6578fe
Song Huang <dr.guangtou@gmail.com>
2015-04-27 22:58:45 +0900

    add coaddBatchCutFull

--- py/hscCoadd/coaddBatchCutout.py
+++ py/hscCoadd/coaddBatchCutout.py
@@ -10,23 +10,41 @@ from astropy.io import fits
 import coaddImageCutout as cdCutout
 import coaddColourImage as cdColor
 
+# HSC Pipeline
+import lsst.daf.persistence  as dafPersist
 
-def decideCutoutSize(z):
+
+def decideCutoutSize(z, safe=False):
 
     """
     Decide the typical cutout size for certain redshift
     """
 
     if (z <= 0.15):
-        return 800
+        if safe:
+            return 800
+        else:
+            return 1000
     if (z > 0.15) and (z < 0.25):
-        return 500
+        if safe:
+            return 500
+        else:
+            return 650
     if (z > 0.25) and (z < 0.35):
-        return 400
+        if safe:
+            return 400
+        else:
+            return 500
     if (z > 0.35) and (z < 0.45):
-        return 350
+        if safe:
+            return 350
+        else:
+            return 400
     if (z > 0.45):
-        return 300
+        if safe:
+            return 300
+        else:
+            return 350
 
 
 def parseInputCatalog(list, sizeDefault=300, idField='id',
@@ -210,6 +228,105 @@ def coaddBatchCutout(root, inCat, size=100, filter='HSC-I', prefix='coadd_cutout
                                          prefix=newPrefix, name=name,
                                          info1=info1, info2=info2, info3=info3,
                                          min=min, max=max, Q=Q)
+    if not onlyColor:
+        logMatch.close()
+
+
+def coaddBatchCutFull(root, inCat, size=100, filter='HSC-I', prefix='coadd_cutout',
+                      idField='id', raField='ra', decField='dec', colorFilters='gri',
+                      sizeField='cutout_size', zCutoutSize=False, zField=None,
+                      verbose=True, noColor=False, onlyColor=False,
+                      infoField1=None, infoField2=None,
+                      min=-0.0, max=0.72, Q=15):
+    """
+    Givin an input catalog with RA, DEC information, generate HSC
+    coadd cutout images.
+
+    Also have the option to generate (or just generate) a 3-band
+    color image
+    """
+
+    if not os.path.isdir(root):
+        raise Exception("Wrong root directory for data! %s" % root)
+    else:
+        try:
+            butler = dafPersist.Butler(root)
+        except Exception:
+            warnings.warn("### Get not load the Butler!")
+
+    if os.path.exists(inCat):
+        id, ra, dec, size, z, extr1, extr2  = parseInputCatalog(inCat, sizeDefault=size,
+                                                 idField=idField, raField=raField,
+                                                 decField=decField, zField=zField,
+                                                 zCutoutSize=zCutoutSize,
+                                                 infoField1=infoField1,
+                                                 infoField2=infoField2)
+    else:
+        raise Exception("### Can not find the input catalog: %s" % inCat)
+
+
+    if not onlyColor:
+        logFile = prefix + '_match_status.lis'
+        logMatch = open(logFile, 'w')
+
+    nObjs = len(id)
+    if verbose:
+        print "### Will try to get cutout image for %d objects" % nObjs
+
+    for i in range(nObjs):
+
+        if verbose:
+            print "### %d -- ID: %s ; " % (i+1, str(id[1])) + \
+                    "RA: %10.5f DEC %10.5f ; Size: %d" % (ra[i], dec[i], size[i])
+
+        # New prefix
+        newPrefix = prefix + '_' + str(id[i]).strip()
+
+        # Cutout Image
+        if not onlyColor:
+            coaddFound = cdCutout.coaddImageCutFull(root, ra[i], dec[i], size[i],
+                                                     savePsf=True, saveSrc=True,
+                                                     visual=True, filt=filter,
+                                                     prefix=newPrefix, butler=butler)
+            if coaddFound:
+                matchStatus = 'Found'
+            else:
+                matchStatus = 'NoData'
+
+            logMatch.write(str(id[i]) + '   ' + matchStatus + '\n')
+
+        # Color Image
+        # Whether put redshift on the image
+        if (zField is not None) and (z is not None):
+            info1 = "z=%5.3f" % z[i]
+        else:
+            info1 = None
+        # Extra information
+        if (infoField1 is not None) and (extr1 is not None):
+            info2 = str(extr1[i]).strip()
+        else:
+            info2 = None
+        if (infoField2 is not None) and (extr2 is not None):
+            info3 = str(extr2[i]).strip()
+        else:
+            info3 = None
+
+        if onlyColor:
+            name = str(id[i])
+            cdColor.coaddColourImageFull(root, ra[i], dec[i], size[i],
+                                         filt=colorFilters,
+                                         prefix=newPrefix, name=name,
+                                         info1=info1, info2=info2, info3=info3,
+                                         min=min, max=max, Q=Q)
+
+        elif (matchStatus is 'Found'):
+            name = str(id[i])
+            cdColor.coaddColourImageFull(root, ra[i], dec[i], size[i],
+                                         filt=colorFilters,
+                                         prefix=newPrefix, name=name,
+                                         info1=info1, info2=info2, info3=info3,
+                                         min=min, max=max, Q=Q)
+
     if not onlyColor:
         logMatch.close()
 
