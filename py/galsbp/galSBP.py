746c72f526b658f123aace6ecb84bb563638f717
dr.guangtou <dr.guangtou@gmail.com>
2016-05-28 04:46:35 +0900

    Add a --replace option to maskFitsPl

--- py/galsbp/galSBP.py
+++ py/galsbp/galSBP.py
@@ -110,7 +110,7 @@ def convIso2Ell(ellTab, xpad=0.0, ypad=0.0):
     return ells
 
 
-def maskFits2Pl(inputImage, inputMask):
+def maskFits2Pl(inputImage, inputMask, replace=False):
     """
     Convert the FITS mask into the IRAF .pl file.
 
@@ -125,7 +125,10 @@ def maskFits2Pl(inputImage, inputMask):
     """
     Why the hell the .pl mask is not working under
     """
-    outputMask = inputImage.replace('.fits', '.fits.pl')
+    if not replace:
+        outputMask = inputImage.replace('.fits', '.fits.pl')
+    else:
+        outputMask = inputImage.replace('.fits', '.pl')
     if os.path.isfile(outputMask):
         os.remove(outputMask)
     # Convert the fits format mask into pl format.
