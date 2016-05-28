2c99c0909c24dcf3d6a3c9d72bc9b817b24a4d5d
dr.guangtou <dr.guangtou@gmail.com>
2016-05-28 23:27:07 +0900

    bug fix

--- py/hscCoadd/batchForceSbp.py
+++ py/hscCoadd/batchForceSbp.py
@@ -36,11 +36,11 @@ def run(args):
 
         """ Keep a log """
         if args.sample is not None:
-            prefix = prefix + '_' + args.sample
+            logPre = prefix + '_' + args.sample
         if args.imgSub:
-            logFile = prefix + '_force_imgsub_' + filter.strip() + '.log'
+            logFile = logPre + '_force_imgsub_' + filter.strip() + '.log'
         else:
-            logFile = prefix + '_force_img_' + filter.strip() + '.log'
+            logFile = logPre + '_force_img_' + filter.strip() + '.log'
         if not os.path.isfile(logFile):
             os.system('touch ' + logFile)
 
